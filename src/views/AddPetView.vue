c70ceda57e45e5eadeb79e1edf8bf077e2c7adf4
flaviabusa <flaviabusacorrea@gmail.com>
2026-09-22 16:40:55 -0300

    ...

---
+++ src/views/AddPetView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header class="mb-4">
+      <h1 class="text-2xl font-bold">Listagem de Pets</h1>
+      <p class="text-body-secundary mb-0">
+        Listagem dos Pets cadastrados no sitema.
+      </p>
+    </header>
+
+    <RouterLink class="btn btn-primary" :to="{ name: 'novo-pet' }">
+      Adicionar Pet
+    </RouterLink>
+  </div>
+</template>
